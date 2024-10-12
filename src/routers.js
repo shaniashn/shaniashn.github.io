4089fea08296a62ea0c3fd9da895ef5ba5ac4f08
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-10-12 23:32:25 +0700

    Update routers.js

--- src/routers.js
+++ src/routers.js
@@ -1,4 +1,20 @@
 //import components here and create routes
+import HomePage from './components/HomePage.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
-// const router = { }
-//export default router
+
+
+const routes = [ //array of objects
+  {
+    name: 'HomePage',
+    component: HomePage,
+    path: '/'
+  }
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+});
+
+export default router;
