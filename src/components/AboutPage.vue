d7cfe1e46fa5d73f58e163a0c50e2c78e3924889
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-10-27 23:34:56 +0700

    Update AboutPage.vue

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,9 +1,57 @@
 <template>
-  <div class="bar">about</div>
+  <section class="about">
+    <div class="about-bar">
+    <ul>
+      <li v-for="(category, index) in categories" :key="index">{{ category }}</li>
+    </ul>
+  </div>
+  </section>
+
 </template>
 
 <script>
 export default {
-  name: 'AboutPage'
+  name: 'AboutPage',
+  data(){
+    return {
+      categories: ['Experience', 'Achievement', 'Education', 'Skills'],
+    }
+  }
 }
 </script>
+
+<style>
+.about {
+  height: 100vh;
+}
+
+.about-bar {
+  display: flex;
+  justify-content: center;
+  height: 50px;
+  position: relative;
+  margin-bottom: 50px;
+}
+
+.about-bar ul {
+  display: flex;
+  justify-content: space-between;
+  width: 30%;
+  height: 100%;;
+  position: absolute;
+}
+
+.about-bar ul li {
+  font-size: 1.1rem;
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+  height: 100%;
+  font-weight: 500;
+}
+
+.about-bar ul li:hover {
+  background-color: var(--pink-color);
+  cursor: default;
+}
+</style>
