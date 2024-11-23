568e150e102edede9a8de9a94594087875c8a15a
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-11-24 00:43:23 +0700

    Create WelcomeView.vue

---
+++ src/components/WelcomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="welcome" id="home">
+    <div class="welcoming">
+      <div class="welcome-container">
+        <div class="welcome-text">
+        <h1>HI, <span>Sha Nia</span> here!</h1>
+        <h5>an iOS Developer who does design.</h5>
+        <div class="socials">
+          <a href="https://www.linkedin.com/in/shaniasiahaan/" target="_blank"><font-awesome-icon icon="fa-brands fa-linkedin-in" size="2x" /></a>
+          <a href="https://github.com/shaniashn" target="_blank"><font-awesome-icon icon="fa-brands fa-github" size="2x" /></a>
+          <a href="https://dribbble.com/shaniashn" target="_blank"><font-awesome-icon icon="fa-brands fa-dribbble" size="2x" /></a>
+          <a href="https://docs.google.com/document/d/1laLjxe9eH63vIoRfLF0_wORnFrbtDu7MZRh8EoR3urA/edit?tab=t.0" target="_blank"><font-awesome-icon icon="fa-regular fa-file" size="2x" /></a>
+        </div>
+      </div>
+      <div class="welcome-img">
+        <img src="../assets/hero-full.svg" alt="person waving">
+      </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeView',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
