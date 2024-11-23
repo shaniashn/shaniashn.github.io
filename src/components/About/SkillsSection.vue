5d6c360efa6e27b253bf6fe7c5c3241a75f1fc11
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-11-23 23:37:32 +0700

    Update SkillsSection.vue

--- src/components/About/SkillsSection.vue
+++ src/components/About/SkillsSection.vue
@@ -4,6 +4,26 @@
       <img src="../../assets/flower1.svg" alt="">
       <p>Swift</p>
     </div>
+    <div class="item">
+      <img src="../../assets/flower1.svg" alt="">
+      <p>JavaScript</p>
+    </div>
+    <div class="item">
+      <img src="../../assets/flower1.svg" alt="">
+      <p>CSS</p>
+    </div>
+    <div class="item">
+      <img src="../../assets/flower1.svg" alt="">
+      <p>Git</p>
+    </div>
+    <div class="item">
+      <img src="../../assets/flower1.svg" alt="">
+      <p>Adobe Illustrator</p>
+    </div>
+    <div class="item">
+      <img src="../../assets/flower1.svg" alt="">
+      <p>Figma</p>
+    </div>
   </div>
 </template>
 
@@ -17,4 +37,18 @@ export default {
 </script>
 
 <style scoped>
+.skills {
+  width: 100%;
+  display: grid;
+  /* grid-template-columns: 50px 50px; */
+  /* grid-auto-flow: column; */
+  grid-template-columns: repeat(3, 1fr);
+  /* column-gap: 10%; */
+  justify-content: space-around;
+}
+
+.item img {
+  width: 60px;
+  height: auto;
+}
 </style>
