b89e809174a02b9047cb7a12c91363c94b97a404
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-11-26 20:52:30 +0700

    fix: space adjustment

--- src/components/About/SkillsSection.vue
+++ src/components/About/SkillsSection.vue
@@ -2,33 +2,33 @@
   <div class="skills">
     <div class="grid">
       <div class="item">
-      <img src="../../assets/skills/swift.svg" alt="">
-      <p>Swift</p>
-    </div>
-    <div class="item">
-      <img src="../../assets/skills/vue.svg" alt="">
-      <p>Vue</p>
-    </div>
-    <div class="item">
-      <img src="../../assets/skills/javascript.svg" alt="">
-      <p>JavaScript</p>
-    </div>
-    <div class="item">
-      <img src="../../assets/skills/css.svg" alt="">
-      <p>CSS</p>
-    </div>
-    <div class="item">
-      <img src="../../assets/skills/git.svg" alt="">
-      <p>Git</p>
-    </div>
-    <div class="item">
-      <img src="../../assets/skills/adobe-illustrator.svg" alt="">
-      <p>Adobe Illustrator</p>
-    </div>
-    <div class="item">
-      <img src="../../assets/skills/figma.svg" alt="">
-      <p>Figma</p>
-    </div>
+        <img src="../../assets/skills/swift.svg" alt="">
+        <p>Swift</p>
+      </div>
+      <div class="item">
+        <img src="../../assets/skills/vue.svg" alt="">
+        <p>Vue</p>
+      </div>
+      <div class="item">
+        <img src="../../assets/skills/javascript.svg" alt="">
+        <p>JavaScript</p>
+      </div>
+      <div class="item">
+        <img src="../../assets/skills/css.svg" alt="">
+        <p>CSS</p>
+      </div>
+      <div class="item">
+        <img src="../../assets/skills/git.svg" alt="">
+        <p>Git</p>
+      </div>
+      <div class="item">
+        <img src="../../assets/skills/adobe-illustrator.svg" alt="">
+        <p>Adobe Illustrator</p>
+      </div>
+      <div class="item">
+        <img src="../../assets/skills/figma.svg" alt="">
+        <p>Figma</p>
+      </div>
     </div>
   </div>
 </template>
