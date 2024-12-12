50cf7a719d17b1cbe0dd66996d40ede91f4b74bf
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-12-12 23:43:30 +0700

    fix: add new container, add maxwidth 375px

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,28 +1,34 @@
 <template>
   <section class="about" id="about">
-    <div class="about-bar">
-      <ul>
-        <li v-for="(category, index) in categories" :key="index" @click="selectCategory(index)">{{ category }}</li>
-      </ul>
-    </div>
-    <div class="about-view-container">
-      <div class="about-view" v-if="showAbout">
-        <div class="about-img">
-          <img :src="profileImage" alt="shania">
-        </div>
-        <div class="about-desc">
-          <p>Hi once again! </p>
-          <p>I’m always excited to introduce myself as creative developer, since those are two things I always working on.</p>
-          <p>I enjoy building interfaces with rich animations, staying in the flow until I find the perfect moment for animation timing, experimenting with new animation styles, and enhancing them with playful color vibes.</p>
-          <p>One of my works won Apple’s WWDC Swift Student Challenge 2024, where I showcased my passion to combine my crafted designs and give it to life with code.</p>
-          <p>Okay, a little bit TMI, I love K-Pop girlgroups especially Red Velvet, NewJeans and aespa.</p>
-        </div>
+    <div class="about-container">
+      <div class="about-bar">
+        <ul>
+          <li v-for="(category, index) in categories" :key="index" @click="selectCategory(index)">{{ category }}</li>
+        </ul>
       </div>
-      <div class="about-view" v-if="showExp">
-        <ExperienceSection />
-      </div>
-      <div class="about-view" v-if="showSkill">
-        <SkillsSection />
+      <div class="about-view-container">
+        <div class="about-view" v-if="showAbout">
+          <div class="about-img">
+            <img :src="profileImage" alt="shania">
+          </div>
+          <div class="about-desc">
+            <p>Hi once again! </p>
+            <p>I’m always excited to introduce myself as creative developer, since those are two things I always working
+              on.</p>
+            <p>I enjoy building interfaces with rich animations, staying in the flow until I find the perfect moment for
+              animation timing, experimenting with new animation styles, and enhancing them with playful color vibes.
+            </p>
+            <p>One of my works won Apple’s WWDC Swift Student Challenge 2024, where I showcased my passion to combine my
+              crafted designs and give it to life with code.</p>
+            <p>Okay, a little bit TMI, I love K-Pop girlgroups especially Red Velvet, NewJeans and aespa.</p>
+          </div>
+        </div>
+        <div class="about-view" v-if="showExp">
+          <ExperienceSection />
+        </div>
+        <div class="about-view" v-if="showSkill">
+          <SkillsSection />
+        </div>
       </div>
     </div>
   </section>
@@ -36,7 +42,7 @@ import SkillsSection from "./About/SkillsSection.vue";
 
 export default {
   name: 'AboutPage',
-  data(){
+  data() {
     return {
       categories: ['about', 'experience & achievement', 'skills'],
       profileImage: profileImg,
@@ -50,7 +56,7 @@ export default {
     SkillsSection
   },
   methods: {
-    selectCategory(index){
+    selectCategory(index) {
       console.log(this.categories[index]);
       let category = this.categories[index]
       this.showAbout = category == "about" ? true : false;
@@ -72,13 +78,14 @@ export default {
   justify-content: center;
   height: 50px;
   position: relative;
-  margin-bottom: 50px;
+  margin: 50px 0;
 }
 
 .about-bar ul {
   display: flex;
   justify-content: space-between;
-  height: 100%;;
+  height: 100%;
+  ;
   position: absolute;
 }
 
@@ -160,6 +167,25 @@ export default {
     width: 300px;
   }
 } */
+
+@media screen and (max-width: 375px) {
+  .about-container {
+    padding: 0 5%;
+  }
+
+  .about-img {
+    max-width: 100%;
+  }
+
+  .about-view {
+    flex-direction: column;
+  }
+
+  .about-desc {
+    max-width: 100%;
+  }
+
+}
 
 @media screen and (min-width: 992px) {
   .about-view {
