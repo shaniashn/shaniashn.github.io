87074ca3b259279728b4c6b28d49b5c94d392bbb
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-12-15 03:02:03 +0700

    feat: add responsive menu interface

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,23 +1,42 @@
 <template>
   <header>
     <nav id="nav">
-    <ul>
-      <a href="/">
-        shania.shn
-      </a>
-    </ul>
-    <ul>
-      <a href="#portfolio"><li>projects</li></a>
-      <a href="#about"><li>about</li></a> 
-      <a href="https://read.cv/shaniashn" target="_blank"><li>resume</li></a> 
-    </ul>
-  </nav>
+      <ul>
+        <a href="/">
+          shania.shn
+        </a>
+      </ul>
+      <ul>
+        <font-awesome-icon icon="fa-solid fa-bars" size="2x" v-show="!showMenu" @click="toggleMenu"/>
+        <font-awesome-icon icon="fa-solid fa-xmark" size="2x" v-show="showMenu" @click="toggleMenu"/>
+        
+        <a href="#portfolio" v-show="showMenu">
+          <li>projects</li>
+        </a>
+        <a href="#about" v-show="showMenu">
+          <li>about</li>
+        </a>
+        <a href="https://read.cv/shaniashn" target="_blank" v-show="showMenu">
+          <li>resume</li>
+        </a>
+      </ul>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
   name: 'NavigationBar',
+  data() {
+    return {
+      showMenu: false
+    }
+  },
+  methods: {
+    toggleMenu() {
+      return this.showMenu = !this.showMenu
+    }
+  }
 }
 </script>
 
@@ -86,4 +105,40 @@ nav ul:last-child {
   width: 30%;
   right: 10%;
 }
+
+nav ul:last-child svg {
+  display: none;
+}
+
+@media screen and (max-width: 769px) {
+
+  nav ul:last-child {
+    width: 100%;
+    right: 0%;
+    flex-direction: column;
+    /* height: 200px; */
+    justify-content: space-evenly;
+    /* padding-top: 20px; */
+    align-items: center;
+    /* background-color: rgb(255, 248, 243, 0.5); */
+    /* backdrop-filter: blur(5px); */
+  }
+
+  nav ul:last-child svg {
+    display: flex;
+    width: 50px;
+    padding: 0 0 5% 0;
+    align-self: end;
+  }
+
+  nav ul:last-child a{
+    padding: 3%;
+  }
+
+  nav ul:last-child svg:first-child {
+    padding: 0;
+  }
+}
+
+
 </style>
